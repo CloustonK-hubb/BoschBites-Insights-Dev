aed15bdc706fcf132ab8416d44a4df78c0e19cb9
CloustonK-hubb <27040259@sun.ac.za>
2026-09-25 14:12:44 +0000

    added CRUD functionality against a fake API

---
+++ my-refine-app/src/providers/data-providers.js
@@ -0,0 +1,37 @@
+//will replace with Supabase data provider API link 
+const API_URL ="https://fake.api-rest.refine.dev";
+
+//establishing CRUD functionality for the data provider 
+export const dataProvider = {
+    getOne: ()=>{
+        throw new Error("Not implemented")
+    },
+    getMany: () => {
+    throw new Error("Not implemented");
+    },
+    update: () => {
+    throw new Error("Not implemented");
+    },
+    updateMany: () => {
+    throw new Error("Not implemented");
+    },
+    getList: () => {
+    throw new Error("Not implemented");
+    },
+    create: () => {
+    throw new Error("Not implemented");
+    },
+    createMany: () => {
+    throw new Error("Not implemented");
+    },
+    delete: () => {
+    throw new Error("Not implemented");
+    },
+    deleteMany: () => {
+    throw new Error("Not implemented");
+    },
+    getApiUrl: () => {
+    throw new Error("Not implemented");
+    },
+
+}
